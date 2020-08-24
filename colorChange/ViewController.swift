9f4af8f11913ec42ddf6d8623888a2a75ef50643
leta <naneecup@gmail.com>
2020-08-24 16:45:00 +0800

    ratio shadow and cornerRadius

--- colorChange/ViewController.swift
+++ colorChange/ViewController.swift
@@ -63,12 +63,18 @@ class ViewController: UIViewController {
         bottomBlueSlider.value = Float.random(in: 0...1)
         bottomAlphaSlider.value = Float.random(in: 0...1)
         gradientMiddleSlider.value = Float.random(in: 0...1)
+        cornerRadiusSlider.value = Float.random(in: 0...80)
+        shadowSlider.value = Float.random(in: 0...10)
         
         gradientLayer.frame = gradientView.bounds
         gradientLayer.colors = [CGColor(red: CGFloat(topRedSlider.value), green: CGFloat(topGreenSlider.value), blue: CGFloat(topBlueSlider.value), alpha: CGFloat(topAlphaSlider.value)),CGColor(red: CGFloat(bottomRedSlider.value), green: CGFloat(bottomGreenSlider.value), blue: CGFloat(bottomBlueSlider.value), alpha: CGFloat(bottomAlphaSlider.value))]
         gradientLayer.locations = [0, NSNumber(value: gradientMiddleSlider.value) , 1]
         gradientView.layer.addSublayer(gradientLayer)
-        
+        totoroImageView.clipsToBounds = true
+        totoroImageView.layer.cornerRadius = CGFloat(cornerRadiusSlider.value)
+        mainView.layer.cornerRadius = CGFloat(cornerRadiusSlider.value)
+        mainView.layer.masksToBounds = false
+        mainView.layer.shadowRadius = CGFloat(shadowSlider.value)
     }
     
 
